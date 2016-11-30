2d43df4bad00252132a424a1554e391d7f6fa28d
vieck <vieck@purdue.edu>
2016-11-29 19:58:21 -0500

    Created blank activity

---
+++ app/src/main/java/edu/purdue/cs/sigapp/studytables/classes/activity/ClassesActivity.java
@@ -0,0 +1,15 @@
+package edu.purdue.cs.sigapp.studytables.classes.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.purdue.cs.sigapp.studytables.R;
+
+public class ClassesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classes);
+    }
+}
