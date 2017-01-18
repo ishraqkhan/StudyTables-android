f2b55756ef3b1fbdae56f445a46fdf5840dcbc88
vieck <vieck@purdue.edu>
2017-01-17 19:16:36 -0500

    PurdueTerm Object Added

---
+++ app/src/main/java/edu/purdue/cs/sigapp/studytables/classes/activity/PurdueTerm.java
@@ -0,0 +1,14 @@
+package edu.purdue.cs.sigapp.studytables.classes.activity;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mvieck on 1/17/17.
+ */
+
+public class PurdueTerm {
+    String termId;
+    String termCode;
+    String name;
+    ArrayList<PurdueClass> classes;
+}
