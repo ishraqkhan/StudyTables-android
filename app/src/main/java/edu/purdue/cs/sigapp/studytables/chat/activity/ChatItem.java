1f5a99ad6ed2c3e6f0551d8d6e18549c86cef630
VinMags16 <vinmags16@gmail.com>
2017-01-23 23:53:06 -0500

    Add framework for chat activity. Still looks rough

---
+++ app/src/main/java/edu/purdue/cs/sigapp/studytables/chat/activity/ChatItem.java
@@ -0,0 +1,8 @@
+package edu.purdue.cs.sigapp.studytables.chat.activity;
+
+/**
+ * Created by Vince on 1/23/2017.
+ */
+
+public class ChatItem {
+}
---
+++ app/src/main/java/edu/purdue/cs/sigapp/studytables/general/adapter/ChatAdapter.java
@@ -0,0 +1,8 @@
+package edu.purdue.cs.sigapp.studytables.general.adapter;
+
+/**
+ * Created by Vince on 1/23/2017.
+ */
+
+public class ChatAdapter {
+}
